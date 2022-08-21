1ab1f3a93a58f68b92492cd362f64018b1bbaaa5
= <asowder3943@gmail.com>
2022-08-21 02:11:12 -0400

    🚸 Added cli prompts to deployment script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-rm -rf $ZSH_CUSTOM
-cp -R . $ZSH_CUSTOM
+echo "\n\nCurrent working directory is $PWD"
+echo "with contents:"
+ls $PWD
+
+echo "\n\nohmyzsh custom directory set to: ~/.oh-my-zsh/custom"
+echo "with contents:"
+ls ~/.oh-my-zsh/custom
+
+echo "\n\nReplacing contents of custom directory with current working directory"
+rm -rf ~/.oh-my-zsh/custom
+mkdir ~/.oh-my-zsh/custom
+cp -R $PWD/ ~/.oh-my-zsh/custom
+# ls ~/.oh-my-zsh/custom
