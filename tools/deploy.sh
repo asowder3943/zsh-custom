7be4dc0b2c2a7b969301e7d90061d3329afeb0de
= <asowder3943@gmail.com>
2022-08-21 02:27:38 -0400

    🍻 removed output and included zshrc config

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
+#!/bin/zsh
 
-echo "\n\nCurrent working directory is $PWD"
-echo "with contents:"
-ls $PWD
-
-echo "\n\nohmyzsh custom directory set to: ~/.oh-my-zsh/custom"
-echo "with contents:"
-ls ~/.oh-my-zsh/custom
-
-echo "\n\nReplacing contents of custom directory with current working directory"
+# deploy cutom directory to zsh custom folder for testing
 rm -rf ~/.oh-my-zsh/custom
 mkdir ~/.oh-my-zsh/custom
-cp -R $PWD/ ~/.oh-my-zsh/custom
-# ls ~/.oh-my-zsh/custom
+cp -R $PWD/ ~/.oh-my-zsh/custom\
+
+# deploy zshrc to home directory for testing
+rm -f ~/.zshrc
+cp .zshrc ~/.zshrc
