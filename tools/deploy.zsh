5ac4e19acf715e8c88c24f1dec5a84b4907373d7
= <asowder3943@gmail.com>
2022-09-14 03:13:41 -0400

    👷 add backups and sourcing to deployment

--- tools/deploy.zsh
+++ tools/deploy.zsh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 # create backups directory
 mkdir -v ~/.zshrc-backups
@@ -6,3 +6,5 @@ mkdir -v ~/.zshrc-backups
 mv -v .zshrc ~/.zshrc-backups/.zshrc_$(date | tr -d ' ' | tr -d ':')
 # move custom config to home directory
 cp -v ~/.oh-my-zsh/custom/.zshrc ~/.zshrc
+# Activate new config
+source ~/.zshrc
