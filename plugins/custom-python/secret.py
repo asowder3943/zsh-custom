24f6a779dc3265069aa321abd71193ebe3dba12f
= <asowder3943@gmail.com>
2022-09-15 14:51:13 -0400

    restricted range to english alphabet

--- plugins/custom-python/secret.py
+++ plugins/custom-python/secret.py
@@ -2,7 +2,7 @@
 import secrets
 
 def main():    
-    allowed_chars = [chr(i) for i in range(0x21, 0x7F)]
+    allowed_chars = [chr(i) for i in range(65, 90)]
     key_length = 60
     print(''.join(secrets.choice(allowed_chars) for i in range(key_length)))
 
